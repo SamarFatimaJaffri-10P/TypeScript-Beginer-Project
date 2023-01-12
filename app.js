8cc0845cd039050293864c3578b1f8af736cd95f
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-12 16:06:40 +0500

    refactor: work with union and literal types

--- app.js
+++ app.js
@@ -1,58 +1,20 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
+function combine(input1, input2, resultConversion // using literal types to force getting one of these strings as inputs
+) {
+    var result;
+    if (typeof input1 == 'number' && typeof input2 == 'number') {
+        result = input1 + input2;
     }
     else {
-        return n1 + n2;
+        result = input1.toString() + input2.toString();
+    }
+    if (resultConversion == 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
     }
 }
-var number1; // it is a good practice to assign type while creating variable in an unassigned way
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, number2, printResult, resultPhrase);
-// objects
-/* if you don't mention the object type, a concrete object is created
- * a concrete is an object that couldn't be modified
- * Don't assign object type to an object, assign {key: type, key: type, ...}
-*/
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];     // this creates a tuple with fixed datatypes
-// } = {
-//     name: 'Sam',
-//     age: 22,
-//     hobbies: ['Coding', 'Learning'],
-//     role: [2, 'author'],
-// };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 5] = "ADMIN";
-    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
-    Role["AUTHOR"] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Sam',
-    age: 22,
-    hobbies: ['Coding', 'Learning'],
-    role: Role.ADMIN
-};
-// person.role.push('admin');  // length is not enforced in push
-// person.role = []         // but length is enforced here, we can't assign more or less then 2 element array
-// person.role[1] = 10;     // throws error
-// array and tuples
-var favoriteActivities;
-favoriteActivities = ['Coding', 'Learning'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    // console.log(hobby.map());    // this throw the error
-    console.log(hobby.toUpperCase());
-}
-if (person.role == 5) {
-    console.log('Person is Admin!');
-}
+var combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
---
+++ objs_arrays_enums.ts
@@ -0,0 +1,65 @@
+function add(n1: number, n2: number, showResult: boolean, phrase: string): number | undefined {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
+}
+
+let number1: number;    // it is a good practice to assign type while creating variable in an unassigned way
+number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
+
+const result = add(number1, number2, printResult, resultPhrase);
+
+// objects
+/** if you don't mention the object type, a concrete object is created
+ * a concrete is an object that couldn't be modified
+ * Don't assign object type to an object, assign {key: type, key: type, ...}
+*/
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];     // this creates a tuple with fixed datatypes
+// } = {
+//     name: 'Sam',
+//     age: 22,
+//     hobbies: ['Coding', 'Learning'],
+//     role: [2, 'author'],
+// };
+
+enum Role {
+    ADMIN = 5,
+    READ_ONLY,
+    AUTHOR = 'AUTHOR',
+}
+
+const person = {
+    name: 'Sam',
+    age: 22,
+    hobbies: ['Coding', 'Learning'],
+    role: Role.ADMIN,
+};
+
+// person.role.push('admin');  // length is not enforced in push
+// person.role = []         // but length is enforced here, we can't assign more or less then 2 element array
+// person.role[1] = 10;     // throws error
+
+// array and tuples
+let favoriteActivities: string[];
+favoriteActivities = ['Coding', 'Learning']
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    // console.log(hobby.map());    // this throw the error
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role == 5){
+    console.log('Person is Admin!');
+}
